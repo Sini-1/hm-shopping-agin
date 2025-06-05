41c5e136fcaa1bc780090f1a27d1e23b7eb2f4cb
liu-gitee888 <liu002386@163.com>
2025-06-05 10:36:56 +0800

    three

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -8,6 +8,7 @@
       <van-tabbar-item icon="bag-o">购物车</van-tabbar-item>
       <van-tabbar-item icon="setting-o">我的</van-tabbar-item>
      </van-tabbar>
+     <div>这个tabbar要在div内部展现，自己是不具备块标签的实力的哈</div>
   </div>
 
 </template>
