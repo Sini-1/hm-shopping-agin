683b6eef4e209d4dfd8dff0d7b6c14628154dc3a
liu-gitee888 <liu002386@163.com>
2025-06-06 10:35:49 +0800

    换行符设置及路由配置与导航栏配置

--- src/router/index.js
+++ src/router/index.js
@@ -7,13 +7,26 @@ import Pay from '@/views/pay'
 import Search from '@/views/search'
 import SearchList from '@/views/search/list.vue'
 import Prodetail from '@/views/prodetail'
+import Home from '@/views/layout/home.vue'
+import Cart from '@/views/layout/cart.vue'
+import Category from '@/views/layout/caregory.vue'
+import User from '@/views/layout/user.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
     { path: '/login', component: Login },
-    { path: '/', component: Layout },
+    {
+      path: '/',
+      component: Layout,
+      children: [
+        { path: '/home', component: Home },
+        { path: '/category', component: Category },
+        { path: '/cart', component: Cart },
+        { path: '/user', component: User }
+      ]
+    },
     { path: '/myorder', component: MyOrder },
     { path: '/pay', component: Pay },
     { path: '/search', component: Search },
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -2,11 +2,11 @@
   <div>
     <!-- 二级路由出口 -->
      <router-view/>
-     <van-tabbar active-color="#ee0a24" inactive-color="#000">
-      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
-      <van-tabbar-item icon="apps-o">分类页</van-tabbar-item>
-      <van-tabbar-item icon="bag-o">购物车</van-tabbar-item>
-      <van-tabbar-item icon="setting-o">我的</van-tabbar-item>
+     <van-tabbar route active-color="#ee0a24" inactive-color="#000">
+      <van-tabbar-item to="/home" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="/category" icon="apps-o">分类页</van-tabbar-item>
+      <van-tabbar-item to="/cart" icon="bag-o">购物车</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="setting-o">我的</van-tabbar-item>
      </van-tabbar>
      <div>这个tabbar要在div内部展现，自己是不具备块标签的实力的哈</div>
   </div>
